43ba087fec5f6fea20f0a0b679da60b2e8752a90
iss\Davis.Sylvester <davis@sylvesterllc.com>
2015-02-06 12:29:19 -0600

    Added UserAccount Service and Supporting classes

---
+++ assets/scripts/custom/Helpers/Admin/UserAccount.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var UserAccount = function(id, firstName, lastName, email, passwd){
+    this.ID = id;
+    this.FirstName = firstName;
+    this.LastName = lastName;
+    this.EMail = email;
+    this.Password = passwd;
+};
+
